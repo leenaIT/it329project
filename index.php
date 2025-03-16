24837e5586e90a3a6ed15df753e67db68d1e9efa
LeenaIT <161076056+leenaIT@users.noreply.github.com>
2025-03-17 02:46:48 +0300

    k

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  $host = "localhost";
+    $database = "medora";
+    $user = "root";
+    $pass = "root";
+    $port = "8889";
+    
+    // إنشاء اتصال بقاعدة البيانات
+    $connection = mysqli_connect($host, $user, $pass, $database,$port);
+    
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }else
+header("Location: signup.php"); // أو أي صفحة رئيسية تريدها
+exit();
+?>
+
+
+?>
+
+
+
